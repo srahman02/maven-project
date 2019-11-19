9848c9f39fe3f30feed0db62518d0e161fd9e357
Rahman <sojibur.rahman@accenture.com>
2019-11-18 22:27:21 -0500

    Warning fixed

--- src/main/java/com/gotocloudapps/GotoCloudAppApplication.java
+++ src/main/java/com/gotocloudapps/GotoCloudAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GotoCloudAppApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(GotoCloudAppApplication.class, args);
 	}
 
